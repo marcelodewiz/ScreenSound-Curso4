63da6b96e3d7f8f788b620682fc9ae9d8fa902a8
Marcelo Migotto <marcelo.dewiz@gmail.com>
2026-03-19 23:52:22 -0300

    feat: adicionado endpoint para retornar a nota do artista

--- ScreenSound.API/Endpoints/ArtistasExtensions.cs
+++ ScreenSound.API/Endpoints/ArtistasExtensions.cs
@@ -108,8 +108,33 @@ public static class ArtistasExtensions
 
             return Results.Created();
         });
+
+        groupBuilder.MapGet("{id}/avaliacao", (
+        int id,
+        HttpContext context,
+        [FromServices] DAL<Artista> dalArtista,
+        [FromServices] DAL<PessoaComAcesso> dalPessoa
+        ) =>
+            {
+                var artista = dalArtista.RecuperarPor(a => a.Id == id);
+                if (artista is null) return Results.NotFound();
+                var email = context.User.Claims
+                    .FirstOrDefault(c => c.Type.Equals(ClaimTypes.Email))?.Value
+                    ?? throw new InvalidOperationException("Não foi encontrado o email da pessoa logada");
+
+                var pessoa = dalPessoa.RecuperarPor(p => p.Email!.Equals(email))
+                    ?? throw new InvalidOperationException("Não foi encontrado o email da pessoa logada");
+
+                var avaliacao = artista
+                    .Avaliacoes
+                    .FirstOrDefault(a => a.ArtistaId == id && a.PessoaId == pessoa.Id);
+
+                if (avaliacao is null) return Results.Ok(new AvaliacaoArtistaResponse(id, 0));
+                else return Results.Ok(new AvaliacaoArtistaResponse(id, avaliacao.Nota));
+            });
         #endregion
     }
+
 
     private static ICollection<ArtistaResponse> EntityListToResponseList(IEnumerable<Artista> listaDeArtistas)
     {
@@ -118,7 +143,15 @@ public static class ArtistasExtensions
 
     private static ArtistaResponse EntityToResponse(Artista artista)
     {
-        return new ArtistaResponse(artista.Id, artista.Nome, artista.Bio, artista.FotoPerfil);
+        return new ArtistaResponse(artista.Id, artista.Nome, artista.Bio, artista.FotoPerfil)
+        {
+
+            Classificacao = artista
+            .Avaliacoes
+            .Select(a => a.Nota)
+            .DefaultIfEmpty()
+            .Average()
+        };
     }
 
   
